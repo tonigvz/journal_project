c877ccd4e7c3f3998e5ed890df2e6fa0f4814659
antonia <goagaantonia@gmail.com>
2022-02-08 18:10:58 +0200

    better structure,can choose file without typing it

--- main.py
+++ main.py
@@ -1,47 +1,87 @@
+# importing datetime to name a new file with the current date,importing inquirer for choosing an option from list,importing os to show the files in folder
 from datetime import date
 import inquirer
 import os
+
+# inquirer list with the actions that the user can perform
+actions = [
+    inquirer.List(
+        "action",
+        message="What would you like to do today?",
+        choices=["Write new page", "Add to existing page", "Read page", "Delete page"],
+    ),
+]
+# launching prompt interface
+answer = inquirer.prompt(actions)
+choice = "".join(value for key, value in answer.items())
+
+
+# creating filename as datetime
 today = date.today()
 filename = f"{today}.txt"
 
-actions = [
-    inquirer.List('action',
-                  message='What would you like to do today?',
-                  choices=['Write new page', 'Add to existing page',
-                           'Read page', 'Delete page']
-                  ),
-]
 
-answer = inquirer.prompt(actions)
+# function that lists the txt files in the folder and the user can choose what to work on from the list of files
+def choose_file():
+    pages = [n for n in os.listdir("D:\Journal") if n.endswith(".txt")]
+    for number, filename in enumerate(pages):
+        print(f"{number} {filename}")
+    return "".join(pages[int(input(f"select file:"))])
 
 
-for key, value in answer.items():
-    if value == 'Write new page':
-        with open(filename, 'w') as f:
-            print('Add your text:')
-            while True:
-                try:
-                    f.writelines(input() + '\n')
-                except EOFError:
-                    break
-    elif value == 'Add to existing page':
-        name = input('Choose file:')
-        with open(name, 'a') as f:
-            print('Add your text:')
-            while True:
-                try:
-                    f.writelines(input() + '\n')
-                except EOFError:
-                    break
-    elif value == 'Read page':
-        name = input('Choose file:')
-        with open(name, 'r') as f:
-            content = f.read()
+# function to write new page
+def write():
+    with open(filename, "w") as f:
+        print("Add your text:")
+        while True:
+            try:
+                f.writelines(input() + "\n")
+            except EOFError:
+                break
 
-        print(content)
-    elif value == 'Delete page':
-        name = input('Choose file:')
-        if input('Are you sure? ') == 'yes':
-            os.remove(name)
-        else:
-            print('Delete cancelled')
+
+# function to append to a page
+def append():
+    name = choose_file()
+    print(f"working on file {name}")
+    with open(name, "a") as f:
+        print("Add your text:")
+        while True:
+            try:
+                f.writelines(input() + "\n")
+            except EOFError:
+                break
+
+
+# function to read a page
+def read():
+    name = choose_file()
+    print(f"working on file {name}")
+    with open(name, "r") as f:
+        content = f.read()
+
+    print(content)
+
+
+# function to delete a page
+def delete():
+    name = choose_file()
+    print(f"working on file {name}")
+    if input("Are you sure? ") == "yes":
+        os.remove(name)
+    else:
+        print("Delete cancelled")
+
+
+# function to perform based on user choice
+def perform(choice):
+    act = {
+        "Write new page": write(),
+        "Add to existing page": append(),
+        "Read page": read(),
+        "Delete page": delete(),
+    }
+    return act.get(choice)
+
+
+perform(choice)
